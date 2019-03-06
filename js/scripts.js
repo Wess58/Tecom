db920ac57c7319918434474951bcc75661760165
Wess58 <wesleymbate@gmail.com>
2019-03-06 16:06:33 +0300

    added function to change Nav color on scroll

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// $(window).scroll(function(){
+//      var scroll = $(window).scrollTop();
+//      if(scroll < 400){
+//          $('.fixed-top').css('background', 'transparent');
+//          $('.nav-link').css('color', '#fff')
+//
+//      } else{
+//          $('.fixed-top').css('background', '#fff');
+//
+//      }
+//  });
