d7052cc81f0abcf1703aafa1bde33ffe9c508c82
Wess58 <wesleymbate@gmail.com>
2019-03-08 13:37:31 +0300

    added BackToTop on scroll function

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,21 @@
 //
 //      }
 //  });
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
