5a8488d2aff30649337e678f766cab4dbb99a65f
Wess58 <wesleymbate@gmail.com>
2019-04-05 12:55:02 +0300

    added Parallax JS Lib

---
+++ js/paraLasic.1.1.js
@@ -0,0 +1,23 @@
+/**
+	* ParaLasic
+	* Version: 1.1
+	* Author: Alex Lupóz
+	* Author URI: https://github.com/alexlupoz
+	* Dependencies: Jquery
+
+	Description:
+
+	A lightweight and very simple Parallax Effect plugin made with Jquery
+
+**/
+
+jQuery(window).on('scroll', function () {
+
+	/* PARALLAX EFFECT ON HEADER */
+	$(window).scroll(function() {
+		var scrollTop = $(window).scrollTop();
+		var paralasicValue = $('.paralasic').attr('data-paralasic');
+		$('.paralasic').css('background-position', 'center top -' + scrollTop * paralasicValue + 'px'); /* 1.8 - default value. Increase less to go stopping the effect */
+	});
+
+});
